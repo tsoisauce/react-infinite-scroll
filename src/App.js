af6b4ff3f9338abf675cdc98a961c2bd113f0486
Kelvin Tsoi <ktsoi@wearfigs.com>
2019-06-15 16:34:02 -0700

    ajax call to json place holder for data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  componentWillMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log('errior: ', err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
       <h1>Infinite Scroll Challenge</h1>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
