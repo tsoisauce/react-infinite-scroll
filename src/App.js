0f160b14f2bbabe6387f158cb0e56e22cdaedb7d
Kelvin Tsoi <ktsoi@wearfigs.com>
2019-06-17 23:40:40 -0700

    add basic ajax call and render out list of reponse data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import './App.css';
-
 class App extends Component {
-  componentWillMount() {
-    this.loadData();
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
   }
 
-  loadData() {
+  componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log('errior: ', err)
-      })
+    .then((response) => {
+      this.setState({
+        isLoaded: true,
+        items: response.data
+      });
+    })
+    .catch((err) => {
+      console.log('errior: ', err)
+    })
   }
 
   render() {
-    return (
-      <div className="App">
-      <h1>Infinite Scroll Challenge</h1>
-    </div>
-    )
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="App">
+          <h1>Infinite Scroll Challenge</h1>
+          <div>
+            <ul>
+              {items.map(item => (
+                <li key={item.id}>
+                  {item.title}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      );
+    }
   }
 }
+
 
 export default App;
